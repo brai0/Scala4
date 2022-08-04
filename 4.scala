a06bd80243920f16f45121ccc5363cdf41262a33
brai0 <98900545+brai0@users.noreply.github.com>
2022-08-04 13:50:43 +0530

    Update 4.scala

--- 4.scala
+++ 4.scala
@@ -1,53 +1,25 @@
-object scalasort {
+abstract class Notification
 
-  def sort(a: List[Int]): List[Int] = {
-    if (a.length < 2) a
-    else {
-      val pivot = a(a.length / 2)
-      sort(a.filter(_ < pivot)) :::
-      a.filter(_ == pivot) :::
-      sort(a.filter(_ > pivot))
-    }
+case class SMS(mobile:String, msg: String) extends Notification 
+case class Email(emailAddr: String, subject: String, body: String) extends Notification 
+
+object temp{
+def showNotification(notification: Notification): String = {
+  notification match {
+    case Email(emailAddr, subject, _) =>
+      s"You got an email from $emailAddr with subject: $subject"
+    case SMS(number, message) =>
+      s"You got an SMS from $number! Message: $message"
   }
-
-  def sort1(a:Array[Int])  {
-  
-  	def swap ( l:Int,r:Int) {
-  		val t = a(l) ; a(l) = a (r) ; a(r) = t
-  		}
-  		
-  	def sort2(l:Int, r:Int) {
-  		val pivot = a((l+r)/2)
-		var i =l
-		var j = r
-		while (i <= j) {
-			while (a(i) < pivot ) i += 1
-			while (a(j) > pivot) j -= 1
-			if (i <= j) {
-				swap (i,j)
-				i += 1
-				j -= 1
-				}
-			}
-			if (l< j) sort2(l,j)
-			if (j < r) sort2(i,r)
-		}
-	if (a.length > 0)
-		sort2(0,a.length-1)
 }
 
-  def main(args: Array[String]) {
+def main(args:Array[String]): Unit = {
+val someSms = SMS("12345", "Are you there?")
+val someEmail = Email("subhrajit.b@nmit.ac.in", "Big Data Course Syllabus",
+  "Intro to Big Data, NOSQL Databases, Spark RDDs, SQL, Streaming")
 
-    val xs = List(6, 2, 8, 5, 1)
-    println(xs)
-    println("Sorted List using the functional style")
-    println(sort(xs))
+println(showNotification(someSms))  
 
-    val xs2 = Array(61,21,81,51,11)
-    sort1(xs2)
-    println("Sorted array using Imperative method")
-	for (ele <- xs2)
-		println(ele)
-
-  }
+println(showNotification(someEmail)) 
+} 
 }
